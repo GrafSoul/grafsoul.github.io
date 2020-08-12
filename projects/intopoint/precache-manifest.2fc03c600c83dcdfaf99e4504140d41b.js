78212cb9ec8b3eec04fce08b825059b4e400c461
GrafSoul <grafsoul@gmail.com>
2020-08-12 12:55:36 +0500

    feat: Added Timers and InToPoint projects.

---
+++ projects/intopoint/precache-manifest.2fc03c600c83dcdfaf99e4504140d41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d255477bd21f10934c81bf1d285afc66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7523461dfeef895eb4c0",
+    "url": "/static/css/main.380442f2.chunk.css"
+  },
+  {
+    "revision": "8045609d1a239c29ea0d",
+    "url": "/static/js/2.92541db3.chunk.js"
+  },
+  {
+    "revision": "9967c6cecbbc00bb7ae0826d8401f406",
+    "url": "/static/js/2.92541db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7523461dfeef895eb4c0",
+    "url": "/static/js/main.cc0ed5f0.chunk.js"
+  },
+  {
+    "revision": "fa7acdef70903e4038b3",
+    "url": "/static/js/runtime-main.6400d03d.js"
+  }
+]);
