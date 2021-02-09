e78a540ec01364589c67d6ac7c7bbcdd9a37c7fe
GrafSoul <grafsoul@gmail.com>
2021-02-09 14:16:28 +0500

    feat: Added language switching functionality.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,16 +1,24 @@
 // ========================================================================================
-// Preloader
-// ========================================================================================
-$(window).on('load', function () {
-    var $preloader = $('.preloader-pages'),
-        $spinner = $preloader.find('.preloader-pages__dynamic-element');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
-
-// ========================================================================================
 // Scripts for Site
 // ========================================================================================
-$( document ).ready(function(){
+(function () {
+    $('.russian').click(function () {
+        console.log('russian');
+        $('.en').fadeOut('slow', function () {
+            $('.ru').fadeIn('slow');
+        });
 
-});
+        $('.russian').removeClass('active');
+        $('.english').addClass('active');
+    });
+
+    $('.english').click(function () {
+        console.log('english');
+        $('.ru').fadeOut('slow', function () {
+            $('.en').fadeIn('slow');
+        });
+
+        $('.english').removeClass('active');
+        $('.russian').addClass('active');
+    });
+})();
